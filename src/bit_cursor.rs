a9fd6c896ef306e43e42ce3910c6ef0493a840ef
bbaldino <bbaldino@gmail.com>
2025-04-07 22:07:08 -0700

    wip: using asref/asmut.  works for most but not vec<u8> or &[u8]

--- src/bit_cursor.rs
+++ src/bit_cursor.rs
@@ -7,7 +7,6 @@ use bitvec::{
     access::BitSafeU8,
     order::Msb0,
     slice::BitSlice,
-    vec::BitVec,
     view::{AsBits, AsMutBits},
 };
 
@@ -102,53 +101,6 @@ impl AsRef<BitSlice<u8, Msb0>> for BytesMutWrapper {
     }
 }
 
-// impl BitCursor<BitVec<u8, Msb0>> {
-//     /// Create a BitCursor from a [`Vec<u8>`]
-//     pub fn from_vec(data: Vec<u8>) -> Self {
-//         Self {
-//             inner: BitVec::from_vec(data),
-//             pos: 0,
-//         }
-//     }
-//
-//     /// Get the data between the current cursor position and the end of the data as a [`BitSlice`]
-//     pub fn remaining_slice(&self) -> &BitSlice<u8, Msb0> {
-//         let len = self.pos.min(self.inner.capacity() as u64);
-//         &self.inner.as_bitslice()[(len as usize)..]
-//     }
-//
-//     /// Get the data between the current cursor position and the end of the data as a mutable [`BitSlice`]
-//     pub fn remaining_slice_mut(&mut self) -> &mut BitSlice<u8, Msb0> {
-//         let start = self.pos.min(self.inner.capacity() as u64);
-//         &mut self.inner.as_mut_bitslice()[(start as usize)..]
-//     }
-//
-//     /// Returns true if the remaining slice is empty
-//     pub fn is_empty(&self) -> bool {
-//         self.pos >= self.remaining_slice().len() as u64
-//     }
-// }
-
-// impl BitCursor<&BitSlice<u8, Msb0>> {
-//     /// Get the data between the current cursor position and the end of the data as a [`BitSlice`]
-//     pub fn remaining_slice(&self) -> &BitSlice<u8, Msb0> {
-//         let len = self.pos.min(self.inner.len() as u64);
-//         &self.inner[(len as usize)..]
-//     }
-//
-//     pub fn is_empty(&self) -> bool {
-//         self.pos >= self.remaining_slice().len() as u64
-//     }
-// }
-
-// impl BitCursor<&[u8]> {
-//     pub fn remaining_slice(&self) -> &BitSlice<u8, Msb0> {
-//         // Here we have to mulitply the slice length by 8, since it's in bytes
-//         let len = self.pos.min((self.inner.len() * 8) as u64);
-//         &self.inner.view_bits::<Msb0>()[(len as usize)..]
-//     }
-// }
-
 impl<T> Clone for BitCursor<T>
 where
     T: Clone,
@@ -199,66 +151,12 @@ where
     }
 }
 
-// impl Read for BitCursor<BitVec<u8, Msb0>> {
-//     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-//         if self.pos % 8 != 0 {
-//             return Err(std::io::Error::new(
-//                 std::io::ErrorKind::Other,
-//                 "Attempted byte-level read when not on byte boundary",
-//             ));
-//         }
-//         match self.remaining_slice().read(buf) {
-//             Ok(n) => {
-//                 self.pos += (n * 8) as u64;
-//                 Ok(n)
-//             }
-//             Err(e) => Err(e),
-//         }
-//     }
-// }
-
-// impl Read for BitCursor<&[u8]> {
-//     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-//         if self.pos % 8 != 0 {
-//             return Err(std::io::Error::new(
-//                 std::io::ErrorKind::Other,
-//                 "Attempted byte-level read when not on byte boundary",
-//             ));
-//         }
-//         match self.remaining_slice().read(buf) {
-//             Ok(n) => {
-//                 self.pos += (n * 8) as u64;
-//                 Ok(n)
-//             }
-//             Err(e) => Err(e),
-//         }
-//     }
-// }
-//
-// impl Read for BitCursor<&BitSlice<u8, Msb0>> {
-//     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-//         if self.pos % 8 != 0 {
-//             return Err(std::io::Error::new(
-//                 std::io::ErrorKind::Other,
-//                 "Attempted byte-level read when not on byte boundary",
-//             ));
-//         }
-//         match self.remaining_slice().read(buf) {
-//             Ok(n) => {
-//                 self.pos += (n * 8) as u64;
-//                 Ok(n)
-//             }
-//             Err(e) => Err(e),
-//         }
-//     }
-// }
-
 impl<T> BitRead for BitCursor<T>
 where
     T: AsRef<BitSlice<u8, Msb0>>,
 {
     fn read_bits(&mut self, buf: &mut [nsw_types::u1]) -> std::io::Result<usize> {
-        let n = BitRead::read_bits(&mut self.inner.as_ref(), buf)?;
+        let n = BitRead::read_bits(&mut BitCursor::split(self).1, buf)?;
         self.pos += n as u64;
         Ok(n)
     }
@@ -305,7 +203,6 @@ mod test {
     use std::io::Write;
     use std::io::{Seek, SeekFrom};
 
-    use bitvec::view::AsBits;
     use bitvec::{bits, order::Msb0, vec::BitVec};
     use bytes::BufMut;
     use bytes::Bytes;
@@ -322,6 +219,12 @@ mod test {
 
     #[test]
     fn test_read() {
+        let data: Vec<u8> = vec![0b11110000, 0b00001111];
+        let mut cursor = BitCursor::new(data);
+    }
+
+    #[test]
+    fn test_read_bits() {
         let data = BitVec::<u8, Msb0>::from_vec(vec![0b11110000, 0b00001111]);
         let mut cursor = BitCursor::new(data);
 
@@ -339,6 +242,18 @@ mod test {
         assert_eq!(read_buf, [u1::new(1), u1::new(1), u1::new(1), u1::new(1)]);
 
         assert_eq!(cursor.read_bits(&mut read_buf).unwrap(), 0);
+    }
+
+    #[test]
+    fn test_read_bytes() {
+        let data = BitVec::<u8, Msb0>::from_vec(vec![1, 2, 3, 4]);
+        let mut cursor = BitCursor::new(data);
+
+        let mut buf = [0u8; 2];
+        std::io::Read::read(&mut cursor, &mut buf).expect("valid read");
+        assert_eq!(buf, [1, 2]);
+        std::io::Read::read(&mut cursor, &mut buf).expect("valid read");
+        assert_eq!(buf, [3, 4]);
     }
 
     #[test]
@@ -364,36 +279,15 @@ mod test {
         assert_eq!(read_buf, [u1::new(1), u1::new(1)]);
     }
 
-    #[test]
-    fn test_read_bytes() {
-        let data = BitVec::<u8, Msb0>::from_vec(vec![1, 2, 3, 4]);
-        let mut cursor = BitCursor::new(data);
-
-        let mut buf = [0u8; 2];
-        std::io::Read::read(&mut cursor, &mut buf).expect("valid read");
-        assert_eq!(buf, [1, 2]);
-        std::io::Read::read(&mut cursor, &mut buf).expect("valid read");
-        assert_eq!(buf, [3, 4]);
-    }
-
-    // TODO: use AsBits/AsBitsMut instead of AsRef/AsMut?
-    fn foo<T>(value: T)
-    where
-        T: AsBits<u8>,
-    {
-    }
+    // fn foo<T: AsMut<bitvec::slice::BitSlice<bitvec::access::BitSafeU8, Msb0>>>(value: T) {}
 
     #[test]
-    fn test_remaining_slice_u8() {
-        let data: Vec<u8> = vec![0b00001111, 0b10101010];
+    fn test_write_bits() {
+        // let mut buf: Vec<u8> = Vec::with_capacity(4);
+        // foo(buf);
+        // let mut cursor = BitCursor::new(buf);
 
-        foo(&data[..]);
-
-        // let mut cursor = BitCursor::new(&data[..]);
-        // cursor.read_u4().unwrap();
-        //
-        // let (before, after) = cursor.split();
-        // assert_eq!(after, bits![u8, Msb0; 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0]);
+        // buf.write_u4(u4::new(0b1100));
     }
 
     #[test]
